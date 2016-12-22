60d1ffa09bac1ee6af8fe0cb16fd1fd51cac295b
sophiayangx <sopyang@nuevaschool.org>
2016-12-21 18:49:22 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -1,14 +1,4 @@
-//$.ajax({
-//  method: "GET",
-//  url: "https://www.govtrack.us/api/v2/person",
-//  success: function(data) {    
-//    var arr = []
-//    data.objects.forEach(function(object) {
-//      arr.push(object.firstname)
-//      console.log(object.firstname)
-//    })
-//  }
-//})
+var button = $("#button")
 
 button.click(function(){
   $.ajax({
