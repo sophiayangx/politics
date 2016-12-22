8aeba3b38307e643c735b6eaed7d0673e03fddc5
sophiayangx <sopyang@nuevaschool.org>
2016-12-21 19:51:50 -0800

    Classadoo autocommit

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ button.click(function(){
         arr.push(object.firstname)
 
         var items = [object.firstname];
-        var item = items[Math.floor(Math.random(2)*1)]
+        var item = items[Math.floor(Math.random()*15)]
         console.log(item)
 //        list.append("<div>" + item + "</div>")
         
